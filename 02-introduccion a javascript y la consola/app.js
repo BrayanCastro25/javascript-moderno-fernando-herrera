12190cf551d0296365209db21b2267c1e1c8a6fc
Brayan Castro <davidbarbosa0525@gmail.com>
2023-09-08 08:29:11 -0500

    creation of folder introduccion a javascript y la consola

---
+++ 02-introduccion a javascript y la consola/app.js
@@ -0,0 +1,11 @@
+alert("Hola desde app.js");
+
+let a = 10,
+    b = 20,
+    c = 30
+
+console.log('%c Mis Variables', 'color: blue; font-weight: bold');
+console.log(a);
+console.log(b);
+console.log(c);
+
