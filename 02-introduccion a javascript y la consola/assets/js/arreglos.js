a320d58dc08b7197d288f5308eb37cc8d0e5f8cb
Brayan Castro <davidbarbosa0525@gmail.com>
2023-09-10 07:53:19 -0500

    creation file arreglos for get data in sequence of arrays

---
+++ 02-introduccion a javascript y la consola/assets/js/arreglos.js
@@ -0,0 +1,14 @@
+const arregloCosas = [
+    "Hola",
+    123,
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a: 1 },
+    ['X', 'Megaman', 'Zero', [
+        'Dr. Willy',
+        'Woodman'
+    ]]
+];
+
+console.log( arregloCosas[6][3][1] );
