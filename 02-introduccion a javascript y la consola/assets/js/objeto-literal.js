9ea6311d9d8b6624469f57d560978f7cff98a6aa
Brayan Castro <davidbarbosa0525@gmail.com>
2023-09-12 08:44:44 -0500

    objetos en javascript

---
+++ 02-introduccion a javascript y la consola/assets/js/objeto-literal.js
@@ -0,0 +1,18 @@
+let personaje = {
+    nombre: "Tony Stark",
+    codeName: "Ironman",
+    vivo: false,
+    edad: 40,
+    cords: {
+        lat: 34.034,
+        lng: -118.7
+    },
+    trajes: ["Mark I", "Mark V", "Hulkbuster"],
+    direccion: {
+        zip: "10800, 90265",
+        ubicacion: "Malibu, California"
+    }
+};
+
+console.log( "Nombre:", personaje.nombre);
+console.log( "Nombre:", personaje["nombre"]);
