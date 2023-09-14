f839f4f94af29fb5cc2e684f0e2daad6c09a5be1
Brayan Castro <davidbarbosa0525@gmail.com>
2023-09-14 07:59:38 -0500

    más detalles sobre los objetos literales

--- 02-introduccion a javascript y la consola/assets/js/objeto-literal.js
+++ 02-introduccion a javascript y la consola/assets/js/objeto-literal.js
@@ -19,3 +19,22 @@ console.log( "Nombre:", personaje["nombre"]);
 
 console.log( "No. Trajes", personaje.trajes.length );
 console.log( "Último traje", personaje.trajes[ personaje.trajes.length - 1 ] );
+
+delete personaje.edad;
+
+const entriesPares = Object.entries( personaje );
+console.log( entriesPares );
+
+// Con el método Object.freeze
+Object.freeze( personaje );
+
+// Esta propiedad evita que se modifiquen o se creen propiedades
+personaje.dinero = 10000000;
+
+// Pero no evita que se modifiquen propiedades de objetos que contenga dicho objeto
+personaje.direccion.ubicacion = "Costa Rica";
+console.log( personaje );
+
+// Obtener los nombres de las propiedades de un objeto
+const propertyNames = Object.getOwnPropertyNames( personaje );
+console.log({ propertyNames });
