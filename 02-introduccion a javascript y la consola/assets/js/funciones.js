22ca29c355ad49f6c74094242fad29ea86a98868
Brayan Castro <davidbarbosa0525@gmail.com>
2023-09-14 08:42:31 -0500

    retorno de las funciones

--- 02-introduccion a javascript y la consola/assets/js/funciones.js
+++ 02-introduccion a javascript y la consola/assets/js/funciones.js
@@ -14,6 +14,18 @@ const saludarFlecha = ( nombre ) => {
 }
 
 
-saludar( 'Juan', 40 );
-saludarAnonimo( 'Pedro', 32 );
-saludarFlecha( 'Luisa', 53 );
+// saludar( 'Juan', 40 );
+// saludarAnonimo( 'Pedro', 32 );
+// saludarFlecha( 'Luisa', 53 );
+
+// Función de flecha
+const getAleatorio = () => {
+    return Math.random();
+}
+
+console.log( getAleatorio() );
+
+// Función de flecha simplificada
+const getAleatorio2 = () => Math.random();
+
+console.log( getAleatorio2() );
