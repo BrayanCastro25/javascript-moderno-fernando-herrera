76b414dbb514dfd4406155b14425f5c09277750a
Brayan Castro <davidbarbosa0525@gmail.com>
2023-09-24 09:15:58 -0500

    switch

---
+++ 02-introduccion a javascript y la consola/assets/js/switch.js
@@ -0,0 +1,34 @@
+const dia = 0; // 0: Domingo
+
+switch ( dia ) {
+    case 0:
+        console.log("Domingo");
+        break;
+
+    case 1:
+        console.log("Lunes");
+        break;
+    
+    case 2:
+        console.log("Martes");
+        break;
+
+    case 3:
+        console.log("Miercoles");
+        break;
+
+    case 4:
+        console.log("Jueves");
+        break;
+        
+    case 5:
+        console.log("Viernes");
+        break;
+
+    case 6:
+        console.log("Sábado");
+        break;
+
+    default:
+        console.log("Incorrecto, no corresponde a un día de la semana");
+}
