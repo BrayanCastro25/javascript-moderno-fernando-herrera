dafb9189225325e601dcb8f8a7a93cf0f94aa191
Brayan Castro <davidbarbosa0525@gmail.com>
2023-10-11 08:58:59 -0500

    refactorizar - crar carta

---
+++ 04-blackjack-vite/src/blackjack/usecases/crear-carta-html.js
@@ -0,0 +1,17 @@
+
+/**
+ * Crear imagen de a carta en HTML
+ * @param {String} carta 
+ * @returns {HTMLImageElement} Elemento HTML imagen
+ */
+export const crearCartaHTML = ( carta ) => {
+
+    if( !carta ) throw new Error("La carta es un argumento obligatorio");
+
+    // <img class="carta" src="assets/cartas/2C.png">
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.classList.add('carta');
+
+    return imgCarta;
+}
