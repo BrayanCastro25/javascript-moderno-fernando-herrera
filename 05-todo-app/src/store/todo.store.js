4d1d88a66754e249c0a6ea163222a4e0439ab806
Brayan Castro <davidbarbosa0525@gmail.com>
2023-10-17 10:32:06 -0500

    implementar métodos del store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -32,9 +32,31 @@ const loadStore = () => {
 }
 
 
+const getTodos = ( filter = Filters.All ) => {
+
+    switch( filter ){
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+
+        case Filters.Pending:
+            return state.todos.filter( todo => todo.done );
+
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+    }
+
+}
+
+
+
 const addTodo = ( description ) => {
 
-    throw new Error("Not implemented");
+    if( !description ) throw new Error('Description is required');
+
+    state.todos.push( new Todo( description ));
 
 }
 
@@ -48,21 +70,24 @@ const toggleTodo = ( todoId ) => {
 
 const deleteTodo = ( todoId ) => {
 
-    throw new Error("Not implemented");
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
 
 }
 
 
 const deleteCompleted = () => {
 
-    throw new Error("Not implemented");
+    state.todos = state.todos.filter( todo => todo.done );
 
 }
 
-
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
 const selectFilter = ( newFilter = Filters.All ) => {
     
-    throw new Error("Not implemented");
+    state.filter = newFilter;
 
 }
 
@@ -77,6 +102,7 @@ const getCurrentFilter = () => {
 export default {
     initStore,
     loadStore,
+    getTodos,
     addTodo,
     toggleTodo,
     deleteTodo,
