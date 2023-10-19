9c5a7764535c5de6675eee3397070587a22021f9
Brayan Castro <davidbarbosa0525@gmail.com>
2023-10-19 07:45:55 -0500

    crear un nuevo todo

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -5,6 +5,7 @@ import { renderTodos } from './use-cases';
 
 const ElementIDs = {
     TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
 }
 
 
@@ -22,8 +23,25 @@ export const App = ( elementId ) => {
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
-        document.querySelector(elementId).append(app);
+        document.querySelector( elementId ).append( app );
         displayTodos();
     })();
+
+    // Referencias HTML
+    const newDescriptionInput = document.querySelector( ElementIDs.NewTodoInput );
+
+    // Listeners
+    newDescriptionInput.addEventListener('keyup', ( event ) => {
+
+        console.log( event );
+        console.log( event.target.value );
+        if( event.keyCode !== 13 ) return;
+        if( event.target.value.trim().length === 0 ) return;
+
+        todoStore.addTodo( event.target.value.trim() );
+        displayTodos();
+
+        event.target.value = '';
+    })
 
 };
